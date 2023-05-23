f57793e35bfbbf2537ec4078e90b5de3bc818cf3
emoryhubbard <edhthe4th@gmail.com>
2023-05-22 21:51:52 -0400

    updated api docs to use render

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title: 'Comments API by Emory Hubbard',
         description: 'A comments API for the Way of the Server tutorial site. It will provide for access to and storage of individual comments, as well as the associated user accounts.'
     },
-    host: 'localhost:3000',
-    schemes: ['http'],
+    host: 'comments-api-2slc.onrender.com', // be sure to update to render (not localhost)!!!
+    schemes: ['https'], // and to https, not http
 };
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
