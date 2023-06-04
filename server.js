edd0cfa2484da5cc124e3f562597711325d20e05
emoryhubbard <edhthe4th@gmail.com>
2023-06-04 01:51:27 -0400

    Added oauth dependencies and config

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const setHeader = require('./utils').setHeader;
+const passport = require('passport')
+const session = require('express-session');
+//require('./passport')(passport)
 
 const app = express();
+
+// OAuth-related middlewares
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(bodyParser.json());
 app.use(setHeader);
 app.use('/', routes);
